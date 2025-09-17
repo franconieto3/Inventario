2b64a750729d1f45be92c993bc2a8b8d4c33319b
Franco <fgnieto@estudiantes.unsam.edu.ar>
2025-09-16 23:46:45 -0300

    primera maqueta de repositorio

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import logo from './assets/logo.png';
 import './App.css'
 
 import { useEffect, useState } from "react";
@@ -13,7 +13,36 @@ function App() {
       .then(data => setMensaje(data));
   }, []);
 
-  return <h1>{mensaje}</h1>;
+  return (  
+        <>
+          <div className='logo-container'>
+            <img className="logo-img" src={logo} alt="logo"></img>
+            <span className='logo-text'>BIOPROTECE S.A.</span>
+          </div>
+          <div className='title-container'>
+            <div>
+            <p className='products-text'>Productos</p>
+            <p className='products-count'>21 productos</p>
+            </div>
+            <button className='add-button'>Agregar producto</button>
+          </div>
+          <div className='input-container'>
+            <input type='text'></input>
+            <button>Buscar</button>
+          </div>
+          <div className='categories-container'>
+            <div className='categories-header'>Nombre</div>
+            <div className='category'>
+              <i class="material-icons">folder</i>
+              <span>PM-01 - Prótesis de cadera</span>
+            </div>
+            <div className='category'>
+              <i class="material-icons">folder</i>
+              <span>PM-02 - Clavos endomedulares de acero</span>
+            </div>
+          </div>
+          <h1>{mensaje}</h1>
+        </>);
 }
 
 export default App;
